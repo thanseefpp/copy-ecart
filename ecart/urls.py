035677d377ccb1b92afe931eb18a67485a9e4cd9
Programmer <androtsf@gmail.com>
2020-09-18 17:37:51 +0530

    admin dashboard and admin login

--- ecart/urls.py
+++ ecart/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.index,),
     path('login',views.login,name='login'),
+    path('adminhome',views.adminhome,name='adminhome')
 ]
--- ecart/views.py
+++ ecart/views.py
@@ -10,3 +10,6 @@ def index(request):
 def login(request):
     return render(request, 'login.html')
 
+
+def adminhome(request):
+    return render(request, 'adminhome.html')
